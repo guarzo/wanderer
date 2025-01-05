f7bc951258726785806ff1ec407ec04d21e63832
Gustav <Guarzo.Eve@gmail.com>
2025-01-04 20:16:01 -0500

    feat(map): Enables left click select and right click pan and drag

--- assets/js/hooks/Mapper/components/map/Map.tsx
+++ assets/js/hooks/Mapper/components/map/Map.tsx
@@ -101,6 +101,7 @@ interface MapCompProps {
   isShowBackgroundPattern?: boolean;
   isSoftBackground?: boolean;
   theme?: string;
+  isRightPanDrag?: boolean;
 }
 
 const MapComp = ({
@@ -118,6 +119,7 @@ const MapComp = ({
   isShowBackgroundPattern,
   isSoftBackground,
   theme,
+  isRightPanDrag,
   onAddSystem,
 }: MapCompProps) => {
   const { getNode, getNodes } = useReactFlow();
@@ -272,6 +274,8 @@ const MapComp = ({
           maxZoom={1.5}
           elevateNodesOnSelect
           deleteKeyCode={['Delete']}
+          selectionOnDrag={isRightPanDrag}
+          panOnDrag={isRightPanDrag ? [2] : [0, 1, 2]}
           // TODO need create clear example with problem with that flag
           //  if system is not visible edge not drawing (and any render in Custom node is not happening)
           // onlyRenderVisibleElements
