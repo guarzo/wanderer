6908c3c15ae8956ef70d5d25814ec459cfd23668
Gustav <Guarzo.Eve@gmail.com>
2025-01-17 21:01:36 -0500

    feat: add zkill widget

--- assets/js/hooks/Mapper/mapRootProvider/hooks/useMapRootHandlers.ts
+++ assets/js/hooks/Mapper/mapRootProvider/hooks/useMapRootHandlers.ts
@@ -29,10 +29,12 @@ import {
 } from './api';
 
 import { emitMapEvent } from '@/hooks/Mapper/events';
+import { DetailedKill } from '../../types/kills';
 
 export const useMapRootHandlers = (ref: ForwardedRef<MapHandlers>) => {
   const mapInit = useMapInit();
-  const { addSystems, removeSystems, updateSystems, updateSystemSignatures } = useCommandsSystems();
+  const { addSystems, removeSystems, updateSystems, updateSystemSignatures, updateDetailedKills } =
+    useCommandsSystems();
   const { addConnections, removeConnections, updateConnection } = useCommandsConnections();
   const { charactersUpdated, characterAdded, characterRemoved, characterUpdated, presentCharacters } =
     useCommandsCharacters();
@@ -106,6 +108,10 @@ export const useMapRootHandlers = (ref: ForwardedRef<MapHandlers>) => {
 
             case Commands.killsUpdated:
               // do nothing here
+              break;
+
+            case Commands.detailedKillsUpdated:
+              updateDetailedKills(data as Record<string, DetailedKill[]>);
               break;
 
             default:
