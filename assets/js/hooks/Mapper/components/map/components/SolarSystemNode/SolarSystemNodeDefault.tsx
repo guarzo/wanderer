93758d38a934a731dd3153ed79a5fe8dc850490f
Gustav <Guarzo.Eve@gmail.com>
2025-01-22 22:55:53 -0500

    feat: add ticker field for nodes

--- assets/js/hooks/Mapper/components/map/components/SolarSystemNode/SolarSystemNodeDefault.tsx
+++ assets/js/hooks/Mapper/components/map/components/SolarSystemNode/SolarSystemNodeDefault.tsx
@@ -20,9 +20,20 @@ export const SolarSystemNodeDefault = memo((props: NodeProps<MapSolarSystemType>
     <>
       {nodeVars.visible && (
         <div className={classes.Bookmarks}>
-          {nodeVars.labelCustom !== '' && (
+          {(nodeVars.labelCustom !== '' || (nodeVars.ownerTicker !== '' && nodeVars.ownerURL !== '')) && (
             <div className={clsx(classes.Bookmark, MARKER_BOOKMARK_BG_STYLES.custom)}>
-              <span className="[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] ">{nodeVars.labelCustom}</span>
+              {nodeVars.ownerURL && nodeVars.ownerTicker ? (
+                <a
+                  href={nodeVars.ownerURL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]"
+                >
+                  {nodeVars.ownerTicker}
+                </a>
+              ) : (
+                <span className="[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">{nodeVars.labelCustom}</span>
+              )}
             </div>
           )}
 
