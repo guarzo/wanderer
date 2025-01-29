fa30e6066e68ffc07cba49d963ef875959cf2732
Gustav <Guarzo.eve@gmail.com>
2025-01-28 20:33:14 -0700

    even more snap

--- assets/js/hooks/Mapper/components/map/Map.tsx
+++ assets/js/hooks/Mapper/components/map/Map.tsx
@@ -283,7 +283,7 @@ const MapComp = ({
           nodeTypes={nodeTypes}
           connectionMode={connectionMode}
           snapToGrid
-          snapGrid={[45, 45]}
+          snapGrid={[35, 35]}
           nodeDragThreshold={10}
           onNodeDragStop={handleDragStop}
           onSelectionDragStop={handleSelectionDragStop}
