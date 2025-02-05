493f6a460bd2af44c1dfffee203aa91933eab81b
Gustav <Guarzo.Eve@gmail.com>
2025-02-04 20:07:32 -0500

    zoo custom

--- assets/js/hooks/Mapper/components/mapWrapper/MapWrapper.tsx
+++ assets/js/hooks/Mapper/components/mapWrapper/MapWrapper.tsx
@@ -8,7 +8,7 @@ import { ContextMenuSystem, useContextMenuSystemHandlers } from '@/hooks/Mapper/
 import {
   SystemCustomLabelDialog,
   SystemLinkSignatureDialog,
-  SystemSettingsDialog,
+  CustomSystemSettingsDialog,
 } from '@/hooks/Mapper/components/mapInterface/components';
 import classes from './MapWrapper.module.scss';
 import { Connections } from '@/hooks/Mapper/components/mapRootContent/components/Connections';
@@ -170,12 +170,9 @@ export const MapWrapper = () => {
       />
 
       {openSettings != null && (
-        <SystemSettingsDialog systemId={openSettings} visible setVisible={() => setOpenSettings(null)} />
+        <CustomSystemSettingsDialog systemId={openSettings} visible setVisible={() => setOpenSettings(null)} />
       )}
 
-      {openCustomLabel != null && (
-        <SystemCustomLabelDialog systemId={openCustomLabel} visible setVisible={() => setOpenCustomLabel(null)} />
-      )}
 
       {linkSignatureToSystem != null && (
         <SystemLinkSignatureDialog data={linkSignatureToSystem} setVisible={() => updateLinkSignatureToSystem(null)} />
