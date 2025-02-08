2116b676f1a7f6afa301467e9a22b50f653a08ff
Gustav <Guarzo.Eve@gmail.com>
2025-02-08 14:07:10 -0500

    zoo: customizations stable

--- assets/js/hooks/Mapper/components/map/helpers/getThemeBehavior.ts
+++ assets/js/hooks/Mapper/components/map/helpers/getThemeBehavior.ts
@@ -1,4 +1,4 @@
-import { SolarSystemNodeDefault, SolarSystemNodeTheme } from '../components/SolarSystemNode';
+import { SolarSystemNodeDefault, SolarSystemNodeTheme, SolarSystemNodeZoo } from '../components/SolarSystemNode';
 import type { NodeProps } from 'reactflow';
 import type { ComponentType } from 'react';
 import { MapSolarSystemType } from '../map.types';
@@ -6,27 +6,49 @@ import { ConnectionMode } from 'reactflow';
 
 export type SolarSystemNodeComponent = ComponentType<NodeProps<MapSolarSystemType>>;
 
+export interface CustomTags {
+  letters?: string[];
+  digits?: string[];
+  others?: string[];
+}
+
 interface ThemeBehavior {
   isPanAndDrag: boolean;
   nodeComponent: SolarSystemNodeComponent;
   connectionMode: ConnectionMode;
+  customTags?: CustomTags;
 }
 
-const THEME_BEHAVIORS: {
-  [key: string]: ThemeBehavior;
-} = {
+const THEME_BEHAVIORS: { [key: string]: ThemeBehavior } = {
   default: {
     isPanAndDrag: false,
     nodeComponent: SolarSystemNodeDefault,
     connectionMode: ConnectionMode.Loose,
+    customTags: {
+      letters: ['A', 'B', 'C', 'D', 'E', 'F', 'X', 'Y', 'Z'],
+      digits: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+    },
   },
   pathfinder: {
     isPanAndDrag: true,
     nodeComponent: SolarSystemNodeTheme,
     connectionMode: ConnectionMode.Loose,
   },
+  zoo: {
+    isPanAndDrag: true,
+    nodeComponent: SolarSystemNodeZoo,
+    connectionMode: ConnectionMode.Strict,
+    customTags: {
+      others: ['1', '2', '3', '4', '5', '6', '7', '8', '10+', '20+', 'Baiting'],
+    },
+  },
 };
 
-export function getBehaviorForTheme(themeName: string) {
+
+export function getBehaviorForTheme(themeName: string): ThemeBehavior {
   return THEME_BEHAVIORS[themeName] ?? THEME_BEHAVIORS.default;
+}
+
+export function getCustomTagsForTheme(themeName: string): CustomTags {
+  return getBehaviorForTheme(themeName).customTags ?? {};
 }
