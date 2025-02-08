7e26c7266d73a905a220e65509c52c4e51e97f77
Gustav <Guarzo.Eve@gmail.com>
2025-02-08 14:07:19 -0500

    zoo: customizations

---
+++ assets/js/hooks/Mapper/components/map/components/SolarSystemNode/SolarSystemNodeZoo.tsx
@@ -0,0 +1,278 @@
+import { memo } from 'react';
+import { MapSolarSystemType } from '../../map.types';
+import { Handle, Position, NodeProps, useReactFlow } from 'reactflow';
+import clsx from 'clsx';
+import classes from './SolarSystemNodeZoo.module.scss';
+import { PrimeIcons } from 'primereact/api';
+import { useSolarSystemNode, useLocalCounter } from '../../hooks/useSolarSystemLogic';
+import { useZooNames, useZooLabels, useUpdateSignatures } from '../../hooks/useZooLogic';
+import {
+  MARKER_BOOKMARK_BG_STYLES,
+  STATUS_CLASSES,
+  EFFECT_BACKGROUND_STYLES,
+} from '@/hooks/Mapper/components/map/constants';
+import { WormholeClassComp } from '@/hooks/Mapper/components/map/components/WormholeClassComp';
+import { KillsCounter } from './SolarSystemKillsCounter';
+import { LocalCounter } from './SolarSystemLocalCounter';
+
+export const SolarSystemNodeZoo = memo((props: NodeProps<MapSolarSystemType>) => {
+  const nodeVars = useSolarSystemNode(props);
+  useUpdateSignatures(nodeVars.solarSystemId);
+
+  const { getEdges } = useReactFlow();
+  const edges = getEdges();
+  const connectionCount = edges.filter(
+    edge => edge.source === props.id || edge.target === props.id
+  ).length;
+
+  const showHandlers = nodeVars.isConnecting || nodeVars.hoverNodeId === nodeVars.id;
+  const dropHandler = nodeVars.isConnecting ? 'all' : 'none';
+
+  const { unsplashedCount, hasEol, hasGas, isDeadEnd, hasCrit } = useZooLabels(connectionCount, {
+    unsplashedLeft: nodeVars.unsplashedLeft,
+    unsplashedRight: nodeVars.unsplashedRight,
+    systemSigs: nodeVars.systemSigs,
+  });
+
+  const { systemName, customLabel, customName } = useZooNames({
+    temporaryName: nodeVars.temporaryName,
+    solarSystemName: nodeVars.solarSystemName,
+    regionName: nodeVars.regionName,
+    labelCustom: nodeVars.labelCustom,
+    ownerTicker: nodeVars.ownerTicker,
+    isWormhole: nodeVars.isWormhole,
+  }, props);
+
+  const { localCounterCharacters } = useLocalCounter(nodeVars);
+
+  return (
+    <>
+      {nodeVars.visible && (
+        <div className={classes.Bookmarks}>
+          {customLabel !== '' && (
+            <div className={clsx(classes.Bookmark, MARKER_BOOKMARK_BG_STYLES.custom)}>
+              {nodeVars.ownerURL && nodeVars.ownerTicker ? (
+                <a
+                  href={nodeVars.ownerURL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]"
+                >
+                  {customLabel}
+                </a>
+              ) : (
+                <span className="[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] ">{customLabel}</span>
+              )}
+            </div>
+          )}
+
+          {nodeVars.isShattered && (
+            <div className={clsx(classes.Bookmark, MARKER_BOOKMARK_BG_STYLES.shattered)}>
+              <span className={clsx('pi pi-chart-pie', classes.icon)} />
+            </div>
+          )}
+
+          {nodeVars.killsCount && nodeVars.killsCount > 0 && nodeVars.solarSystemId && (
+            <KillsCounter
+              killsCount={nodeVars.killsCount}
+              systemId={nodeVars.solarSystemId}
+              size="lg"
+              killsActivityType={nodeVars.killsActivityType}
+              className={clsx(classes.Bookmark, MARKER_BOOKMARK_BG_STYLES[nodeVars.killsActivityType!])}
+            >
+              <div className={clsx(classes.BookmarkWithIcon)}>
+                <span className={clsx(PrimeIcons.BOLT, classes.icon)} />
+                <span className={clsx(classes.text)}>{nodeVars.killsCount}</span>
+              </div>
+            </KillsCounter>
+          )}
+
+          {unsplashedCount > 0 && (
+            <div className={clsx(classes.Bookmark, MARKER_BOOKMARK_BG_STYLES.unSplashed)}>
+              <div className={clsx(classes.BookmarkWithIcon)}>
+                <span className={clsx("pi pi-bullseye")} style={{ marginRight: '3px', fontSize: '8px' }}></span>
+                <span className={clsx(classes.text, classes.openWhText)}>{unsplashedCount}</span>
+              </div>
+            </div>
+          )}
+
+          {hasEol && (
+            <div className={clsx(classes.Bookmark, MARKER_BOOKMARK_BG_STYLES.eol)}>
+              <span className={clsx('pi pi-stopwatch', classes.icon)} />
+            </div>
+          )}
+
+          {hasGas && (
+            <div className={clsx(classes.Bookmark, MARKER_BOOKMARK_BG_STYLES.gas)}>
+              <span className={clsx('pi pi-cloud', classes.icon)} style={{ color: 'black' }} />
+            </div>
+          )}
+
+          {nodeVars.isWormhole && isDeadEnd && (
+            <div className={clsx(classes.Bookmark, MARKER_BOOKMARK_BG_STYLES.deadend)}>
+              <span className={clsx('pi pi-directions-alt', classes.icon)} />
+            </div>
+          )}
+
+          {hasCrit && (
+            <div className={clsx(classes.Bookmark, MARKER_BOOKMARK_BG_STYLES.crit)}>
+              <span className={clsx('pi pi-info-circle', classes.icon)} />
+            </div>
+          )}
+
+          {nodeVars.labelsInfo.map(x => (
+            <div key={x.id} className={clsx(classes.Bookmark, MARKER_BOOKMARK_BG_STYLES[x.id])}>
+              {x.shortName}
+            </div>
+          ))}
+        </div>
+      )}
+
+      <div
+        className={clsx(
+          classes.RootCustomNode,
+          nodeVars.regionClass && classes[nodeVars.regionClass],
+          nodeVars.status != null ? classes[STATUS_CLASSES[nodeVars.status]] : null,
+          {
+            [classes.selected]: nodeVars.selected,
+          },
+        )}
+      >
+        {nodeVars.visible && (
+          <>
+            <div className={classes.HeadRow}>
+              <div
+                className={clsx(
+                  classes.classTitle,
+                  nodeVars.classTitleColor,
+                  '[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]',
+                )}
+              >
+                {nodeVars.classTitle ?? '-'}
+              </div>
+
+              <div
+                className={clsx(
+                  classes.classSystemName,
+                  'flex-grow overflow-hidden text-ellipsis whitespace-nowrap font-sans',
+                )}
+              >
+                {systemName}
+              </div>
+
+              {nodeVars.isWormhole && (
+                <div className={classes.wormholeContainer}>
+                  <div className={classes.statics}>
+                    {nodeVars.sortedStatics.map(whClass => (
+                      <WormholeClassComp key={whClass} id={whClass} />
+                    ))}
+                  </div>
+                  {nodeVars.effectName !== null && (
+                    <div className={clsx(classes.effect, EFFECT_BACKGROUND_STYLES[nodeVars.effectName])} />
+                  )}
+                </div>
+              )}
+            </div>
+
+            <div className={clsx(classes.BottomRow, 'flex items-center justify-between')}>
+              <div className="flex items-center gap-2">
+                {nodeVars.tag != null && nodeVars.tag !== '' && (
+                  <div className={clsx(classes.tagTitle, 'font-medium')}>{`[${nodeVars.tag}]`}</div>
+                )}
+                <div className={clsx(classes.customName)} title={`${customName ?? ''}`}>
+                  {customName}
+                </div>
+              </div>
+              <div className="flex items-center justify-end">
+                <div
+                  className={clsx('flex items-center gap-1', {
+                    [classes.hasLocalCounter]: nodeVars.charactersInSystem.length > 0,
+                    [classes.countAbove9]: nodeVars.charactersInSystem.length > 9,
+                  })}
+                >
+                  {nodeVars.locked && <i className={clsx(PrimeIcons.LOCK, classes.lockIcon)} />}
+                  {nodeVars.hubs.includes(nodeVars.solarSystemId.toString()) && (
+                    <i className={clsx(PrimeIcons.MAP_MARKER, classes.mapMarker)} />
+                  )}
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+      <div onMouseDownCapture={nodeVars.dbClick} className={classes.Handlers}>
+        <Handle
+          type="target"
+          position={Position.Bottom}
+          style={{
+            width: '100%',
+            height: '100%',
+            background: 'none',
+            cursor: 'cell',
+            pointerEvents: dropHandler,
+            opacity: 0,
+            borderRadius: 0,
+          }}
+          id="whole-node-target"
+        />
+        <Handle
+          type="source"
+          className={clsx(classes.Handle, classes.HandleTop, {
+            [classes.selected]: nodeVars.selected,
+            [classes.Tick]: nodeVars.isThickConnections,
+          })}
+          style={{
+            width: '100%',
+            height: '55%',
+            background: 'none',
+            cursor: 'cell',
+            opacity: 0,
+            borderRadius: 0,
+            visibility: showHandlers ? 'visible' : 'hidden',
+          }}
+          position={Position.Top}
+          id="a"
+        />
+        <Handle
+          type="source"
+          className={clsx(classes.Handle, classes.HandleRight, {
+            [classes.selected]: nodeVars.selected,
+            [classes.Tick]: nodeVars.isThickConnections,
+          })}
+          style={{ visibility: showHandlers ? 'visible' : 'hidden', cursor: 'cell', zIndex: 10 }}
+          position={Position.Right}
+          id="b"
+        />
+        <Handle
+          type="source"
+          className={clsx(classes.Handle, classes.HandleBottom, {
+            [classes.selected]: nodeVars.selected,
+            [classes.Tick]: nodeVars.isThickConnections,
+          })}
+          style={{ visibility: 'hidden', cursor: 'cell' }}
+          position={Position.Bottom}
+          id="c"
+        />
+        <Handle
+          type="source"
+          className={clsx(classes.Handle, classes.HandleLeft, {
+            [classes.selected]: nodeVars.selected,
+            [classes.Tick]: nodeVars.isThickConnections,
+          })}
+          style={{ visibility: showHandlers ? 'visible' : 'hidden', cursor: 'cell', zIndex: 10 }}
+          position={Position.Left}
+          id="d"
+        />
+      </div>
+      <LocalCounter
+        hasUserCharacters={nodeVars.hasUserCharacters}
+        localCounterCharacters={localCounterCharacters}
+        classes={classes}
+        showIcon={false}
+      />
+    </>
+  );
+});
+
+SolarSystemNodeZoo.displayName = 'SolarSystemNodeZoo';
+export default SolarSystemNodeZoo;
