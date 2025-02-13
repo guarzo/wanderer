ae0a8e7b361387a2ba9601da5109f566f206b2cc
Gustav <Guarzo.Eve@gmail.com>
2025-02-12 22:10:27 -0500

    zoo: labels and icons

--- assets/js/hooks/Mapper/components/contexts/ContextMenuSystem/hooks/useLabelsMenu/useLabelsMenu.ts
+++ assets/js/hooks/Mapper/components/contexts/ContextMenuSystem/hooks/useLabelsMenu/useLabelsMenu.ts
@@ -4,7 +4,7 @@ import { useCallback, useRef } from 'react';
 import { SolarSystemRawType } from '@/hooks/Mapper/types';
 import { getSystemById } from '@/hooks/Mapper/helpers';
 import clsx from 'clsx';
-import { LABELS, LABELS_INFO, LABELS_ORDER } from '@/hooks/Mapper/components/map/constants.ts';
+import { LABELS, LABELS_INFO, LABELS_ORDER } from '@/hooks/Mapper/components/map/constants';
 import { GRADIENT_MENU_ACTIVE_CLASSES } from '@/hooks/Mapper/constants.ts';
 import { LabelsManager } from '@/hooks/Mapper/utils/labelsManager.ts';
 
@@ -31,7 +31,7 @@ export const useLabelsMenu = (
   ref.current = { onSystemLabels, systemId, systems, onCustomLabelDialog };
 
   return useCallback(() => {
-    const { onSystemLabels, systemId, systems, onCustomLabelDialog } = ref.current;
+    const { onSystemLabels, systemId, systems } = ref.current;
     const system = systemId ? getSystemById(systems, systemId) : undefined;
     const labels = new LabelsManager(system?.labels ?? '');
 
