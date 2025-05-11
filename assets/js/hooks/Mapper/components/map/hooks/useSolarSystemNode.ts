10f8f014a2a4e8ccc1b394a30f60da6a3d73ead4
Guarzo <guarzo.eve@gmail.com>
2025-05-10 20:01:16 -0400

    zoo: custom stable

--- assets/js/hooks/Mapper/components/map/hooks/useSolarSystemNode.ts
+++ assets/js/hooks/Mapper/components/map/hooks/useSolarSystemNode.ts
@@ -55,7 +55,9 @@ export function useSolarSystemNode(props: NodeProps<MapSolarSystemType>): SolarS
   } = data;
 
   const {
-    storedSettings: { interfaceSettings },
+    storedSettings: {
+      interfaceSettings: { isShowUnsplashedSignatures },
+    },
     data: { systemSignatures: mapSystemSignatures },
   } = useMapRootState();
 
@@ -75,7 +77,6 @@ export function useSolarSystemNode(props: NodeProps<MapSolarSystemType>): SolarS
     solar_system_name,
   } = systemStaticInfo;
 
-  const { isShowUnsplashedSignatures } = interfaceSettings;
   const isTempSystemNameEnabled = useMapGetOption('show_temp_system_name') === 'true';
   const isShowLinkedSigId = useMapGetOption('show_linked_signature_id') === 'true';
   const isShowLinkedSigIdTempName = useMapGetOption('show_linked_signature_id_temp_name') === 'true';
@@ -154,7 +155,7 @@ export function useSolarSystemNode(props: NodeProps<MapSolarSystemType>): SolarS
 
   const hubsAsStrings = useMemo(() => hubs.map(item => item.toString()), [hubs]);
 
-  const nodeVars: SolarSystemNodeVars = {
+  return {
     id,
     selected,
     visible,
@@ -191,8 +192,6 @@ export function useSolarSystemNode(props: NodeProps<MapSolarSystemType>): SolarS
     regionName: region_name,
     solarSystemName: solar_system_name,
   };
-
-  return nodeVars;
 }
 
 export interface SolarSystemNodeVars {
