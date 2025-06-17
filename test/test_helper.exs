ce675a05798f953bc41dc0e51b62bc1cffa424a8
guarzo <guarzo.eve@gmail.com>
2025-06-17 18:29:05 -0400

    tmp

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,6 +2,9 @@
 # Individual tests can disable it using WandererApp.DeprecationTestHelpers.with_legacy_api_disabled/1
 System.put_env("FEATURE_LEGACY_API", "true")
 
+# Load support files first to ensure mocks are available during compilation
+Code.require_file("support/mocks.ex", __DIR__)
+
 # Ensure all applications are started before tests
 {:ok, _} = Application.ensure_all_started(:wanderer_app)
 
@@ -16,4 +19,3 @@ Application.ensure_all_started(:mox)
 
 # Load support files
 Code.require_file("support/test_cleanup.ex", __DIR__)
-Code.require_file("support/mocks.ex", __DIR__)
