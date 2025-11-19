c82036796ab96b95237ecda0f4010e8bee76e7a1
Guarzo <guarzo.eve@gmail.com>
2025-11-19 20:56:10 +0000

    zoo: stable

--- assets/js/hooks/Mapper/mapRootProvider/hooks/useMapRootHandlers.ts
+++ assets/js/hooks/Mapper/mapRootProvider/hooks/useMapRootHandlers.ts
@@ -24,6 +24,8 @@ import {
   CommandUpdateSystems,
   CommandUserSettingsUpdated,
   MapHandlers,
+  CommandReadyCharactersUpdated,
+  CommandAllReadyCharactersCleared,
 } from '@/hooks/Mapper/types/mapHandlers.ts';
 import { ForwardedRef, useImperativeHandle } from 'react';
 
@@ -54,8 +56,15 @@ export const useMapRootHandlers = (ref: ForwardedRef<MapHandlers>) => {
     updateDetailedKills,
   } = useCommandsSystems();
   const { addConnections, removeConnections, updateConnection } = useCommandsConnections();
-  const { charactersUpdated, characterAdded, characterRemoved, characterUpdated, presentCharacters } =
-    useCommandsCharacters();
+  const {
+    charactersUpdated,
+    characterAdded,
+    characterRemoved,
+    characterUpdated,
+    presentCharacters,
+    readyCharactersUpdated,
+    allReadyCharactersCleared,
+  } = useCommandsCharacters();
   const mapUpdated = useMapUpdated();
   const mapRoutes = useRoutes();
   const mapUserRoutes = useUserRoutes();
@@ -172,6 +181,15 @@ export const useMapRootHandlers = (ref: ForwardedRef<MapHandlers>) => {
           case Commands.pingCancelled:
             pingCancelled(data as CommandPingCancelled);
             break;
+
+          case Commands.readyCharactersUpdated:
+            readyCharactersUpdated(data as CommandReadyCharactersUpdated);
+            break;
+
+          case Commands.allReadyCharactersCleared:
+            allReadyCharactersCleared(data as CommandAllReadyCharactersCleared);
+            break;
+
           default:
             console.warn(`JOipP Interface handlers: Unknown command: ${type}`, data);
             break;
@@ -180,5 +198,33 @@ export const useMapRootHandlers = (ref: ForwardedRef<MapHandlers>) => {
         emitMapEvent({ name: type, data });
       },
     };
-  }, []);
+  }, [
+    addComment,
+    addConnections,
+    addSystems,
+    allReadyCharactersCleared,
+    characterActivityData,
+    characterAdded,
+    characterRemoved,
+    characterUpdated,
+    charactersUpdated,
+    mapInit,
+    mapRoutes,
+    mapUpdated,
+    mapUserRoutes,
+    pingAdded,
+    pingCancelled,
+    presentCharacters,
+    readyCharactersUpdated,
+    removeComment,
+    removeConnections,
+    removeSystems,
+    trackingCharactersData,
+    updateConnection,
+    updateDetailedKills,
+    updateLinkSignatureToSystem,
+    updateSystemSignatures,
+    updateSystems,
+    userSettingsUpdated,
+  ]);
 };
